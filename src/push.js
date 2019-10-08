9082a6881243538f1e8ad2de0c8a1a1433f43baa
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-08 20:31:44 -0300

    Registrando service worker para push notification

--- src/push.js
+++ src/push.js
@@ -1,5 +1,43 @@
+import firebase from 'firebase';
+
 export default function(){
 
-    console.log('working');
+
+
+
+   if ('serviceWorker' in navigator){
+
+        navigator.serviceWorker.register('./firebase-messaging-sw.js')
+            .then(registration => {
+
+                const messaging = firebase.messaging();
+
+                let getToken = () => {
+
+                    messaging.getToken()
+                        .then(currentToken => {
+                            if (currentToken){
+                                console.log('Enviar isso para um servidor:', currentToken);
+                                return currentToken;
+                            }
+            
+                            console.warn('Nenhum token está disponível, solicite permissão para gerar um.');
+                        });
+            
+            
+                }
+
+                messaging.useServiceWorker(registration);
+
+                messaging.requestPermission()
+                    .then(() => {
+                        return getToken();
+                    })
+                   
+
+                    
+            })
+            .catch(err => console.warn('Erro', err))
+   }
 
 }
