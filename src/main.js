21e32f6b727f0c9ad0ca02ad2fcefd0e35f7a773
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-08 16:30:48 -0300

    Instalando e carregando firebase e vuefire

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+//import VueFire from 'vuefire';
+import firebase from 'firebase';
+import push from './push';
+
+Vue.use(firebase);
+
+push();
 
 Vue.config.productionTip = false
 
