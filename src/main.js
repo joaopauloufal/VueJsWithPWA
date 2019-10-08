e8e2d508400a502b01a2adffe3cebe298fa7fec4
joao.silva <joao.silva@arapiraca.al.gov.br>
2019-10-08 16:37:16 -0300

    Criando projeto no firebase console e iniciando no vue

--- src/main.js
+++ src/main.js
@@ -6,7 +6,23 @@ import './registerServiceWorker'
 import firebase from 'firebase';
 import push from './push';
 
+
+
 Vue.use(firebase);
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCanSmLwBUkXGSeD3RzeVo20D4LWR5C4eA",
+  authDomain: "vuejs-wpa.firebaseapp.com",
+  databaseURL: "https://vuejs-wpa.firebaseio.com",
+  projectId: "vuejs-wpa",
+  storageBucket: "",
+  messagingSenderId: "97020739860",
+  appId: "1:97020739860:web:01972ba680cdc374f88201",
+  measurementId: "G-MM1Y8CWBFQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 push();
 
